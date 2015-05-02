1976e3cdff1d83cc76126cc9ce214961b049e497
jakofranko <jacob.d.franklin@gmail.com>
2015-05-02 13:05:20 -0700

    Added views and routes for viewing and adding new martial maneuvers

--- routes/martialManeuvers.js
+++ routes/martialManeuvers.js
@@ -2,17 +2,42 @@ var express = require('express');
 var router = express.Router();
 var martialManeuvers = require('../models/martialManeuvers')
 
-/* GET home page. */
+// GET
+// --------------------------------------------------------------
 router.get('/', function(req, res) {
 	if(req.session.role == 'admin')	{
 		martialManeuvers.listMartialManeuvers(function(err, mms) {
 			if(err) throw new Error(err);
 			else {
-				res.render('martialManeuversShow', { title: 'Martial Maneuvers', mms: mms });
+				res.render('martialManeuversShow', { title: 'Martial Maneuvers', mms: mms, session: req.session });
 			}
 		});
+	} else {
+		res.redirect('/dashboard/' + req.session._id);
 	}
-	else {
+});
+
+router.get('/add', function(req, res) {
+	if(req.session.role == 'admin')	{
+		res.render('martialManeuversAdd', { title: 'Add New Martial Maneuver' });
+	} else {
+		res.redirect('/dashboard/' + req.session._id);
+	}
+});
+
+// POST
+// --------------------------------------------------------------
+router.post('/addMartialManeuver', function(req, res) {
+	if(req.session.role == 'admin')	{
+		console.log(req.body);
+		var mm = req.body;
+		martialManeuvers.createMartialManeuver(mm.name, mm.phase, mm.ocv, mm.dcv, mm.cost, mm.effects, function(err, result) {
+			if(err) throw new Error(err);
+			else {
+				res.redirect("/martialManeuvers/");
+			}
+		});
+	} else {
 		res.redirect('/dashboard/' + req.session._id);
 	}
 });
