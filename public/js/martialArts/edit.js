8e0b3b5e188a8f40cc3bd4889868167dd5ac4e59
jakofranko <jacob.d.franklin@gmail.com>
2015-06-19 13:06:19 -0700

    Updated front end to reflect point changes
    https://app.asana.com/0/25694918946217/38406442753575

--- public/js/martialArts/edit.js
+++ public/js/martialArts/edit.js
@@ -3,16 +3,28 @@ $(document).ready(function() {
 	var art		  = $('#art');
 
 	$('.add-maneuver').click(function() {
+		var points = 0;
 		var selected = maneuvers.find(':selected');
+		selected.each(function(i, el) {
+			var data = $(el).data();
+			points += data.cost;
+		});
 		selected.appendTo(art);
 		art.find('option').prop('selected', true);
+		updateSpentPoints(points);
 		scafoldMartialArt();
 	});
 
 	$('.remove-maneuver').click(function() {
+		var points = 0;
 		var selected = art.find(':selected');
+		selected.each(function(i, el) {
+			var data = $(el).data();
+			points += data.cost;
+		});
 		selected.appendTo(maneuvers);
 		art.find('option').prop('selected', true);
+		updateSpentPoints(-points);
 		scafoldMartialArt();
 	});
 });
@@ -22,8 +34,13 @@ function scafoldMartialArt() {
 	
 	newArt.find('option').prop('selected', true);
 	var data = newArt.val();
-	console.log(data); 
 	$.post('/martialManeuvers/newArt/', {mms: data}, function(response) {
 		$("#newArt").html(response);
 	});
+}
+
+function updateSpentPoints(points) {
+	var remainingPoints = $("#remaining-points");
+	var oldPoints = Number(remainingPoints.text());
+	remainingPoints.text(oldPoints - points);
 }
