dadabe90d810e74a47aa555fad006b0a6fdf6cbd
jakofranko <jacob.d.franklin@gmail.com>
2015-06-19 13:07:55 -0700

    Front end now dynamically reflects point changes
    https://app.asana.com/0/25694918946217/38406442753573

--- public/js/martialArts/add.js
+++ public/js/martialArts/add.js
@@ -3,14 +3,26 @@ $(document).ready(function() {
 	var art		  = $('#art');
 
 	$('.add-maneuver').click(function() {
+		var points = 0;
 		var selected = maneuvers.find(':selected');
+		selected.each(function(i, el) {
+			var data = $(el).data();
+			points += data.cost;
+		});
 		selected.appendTo(art);
+		updateSpentPoints(points);
 		scafoldMartialArt();
 	});
 
 	$('.remove-maneuver').click(function() {
+		var points = 0;
 		var selected = art.find(':selected');
+		selected.each(function(i, el) {
+			var data = $(el).data();
+			points += data.cost;
+		});
 		selected.appendTo(maneuvers);
+		updateSpentPoints(points);
 		scafoldMartialArt();
 	});
 });
@@ -24,4 +36,10 @@ function scafoldMartialArt() {
 	$.post('/martialManeuvers/newArt/', {mms: data}, function(response) {
 		$("#newArt").html(response);
 	});
+}
+
+function updateSpentPoints(points) {
+	var remainingPoints = $("#remaining-points");
+	var oldPoints = Number(remainingPoints.text());
+	remainingPoints.text(oldPoints - points);
 }
