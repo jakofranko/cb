3703717c761ce55e77f93134ce078e25f3a686c7
jakofranko <jacob.d.franklin@gmail.com>
2015-07-04 17:53:52 -0700

    New views for character perks
    https://app.asana.com/0/25694918946217/40091735297540

---
+++ public/js/perks/add.js
@@ -0,0 +1,42 @@
+// Script taken from Dudley Storey @dudleystorey and improved by me, Jake Franklin
+// http://demosthenes.info/blog/864/Auto-Generate-Marks-on-HTML5-Range-Sliders-with-JavaScript
+function ticks(element) {
+	if (element.hasAttribute('list') && element.hasAttribute('min') && element.hasAttribute('max') && element.hasAttribute('step')) {
+		var datalist = document.createElement('datalist'),
+		minimum = parseInt(element.getAttribute('min')),
+		step = parseInt(element.getAttribute('step')),
+		maximum = parseInt(element.getAttribute('max'));
+		datalist.id = element.getAttribute('list');
+		for (var i = minimum; i < maximum+step; i = i + step) {
+			datalist.innerHTML +="<option value="+i+"></option>";
+		}
+		element.parentNode.insertBefore(datalist, element.nextSibling);
+	}
+}
+
+function handleInput(optionData) {
+	var input = $('#perk-cost');
+	if(optionData.mincost < optionData.maxcost) {
+		input.attr('type', 'range').attr('min', optionData.mincost).attr('max', optionData.maxcost).val(optionData.mincost).removeClass('form-control').prop('readonly', false);
+		$('#min-cost').text(optionData.mincost);
+		$('#max-cost').text(optionData.maxcost);
+	} else if(optionData.mincost == optionData.maxcost) {
+		input.attr('type', 'number').addClass('form-control').prop('readonly', true).val(optionData.mincost);
+		$('#min-cost').text(null);
+		$('#max-cost').text(null);
+	} else {
+		input.attr('type', 'number').attr('min', optionData.mincost).attr('max', null).val(optionData.mincost).addClass('form-control').prop('readonly', false);
+		$('#min-cost').text(null);
+		$('#max-cost').text(null);
+	}
+	ticks(input[0]);
+}
+$(document).ready(function() {
+	$('#perk-type').change(function() {
+		handleInput($(this).find(':selected').data());
+		$('#perk-cost').trigger('change');
+	});
+	$('#perk-cost').change(function() {
+		$('#perk-total').text(this.value);
+	});
+});
