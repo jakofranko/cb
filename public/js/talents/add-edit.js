e16230865edfb8694cb708713f9f9e3c2957f90e
jakofranko <jacob.d.franklin@gmail.com>
2015-11-01 13:36:39 -0700

    Added views for viewing all talents, and adding new talents
    https://app.asana.com/0/47316382927622/60352960933440
    https://app.asana.com/0/47316382927622/62563300653601
    https://app.asana.com/0/47316382927622/62563300653603

--- public/js/talents/add-edit.js
+++ public/js/talents/add-edit.js
@@ -1,28 +1,33 @@
-$(document).ready(function() {
-	var adders = $('#adders');
-	$('.new-adder').click(function() {
-		var adderNum = $('.adder').length;
-		var formGroup = $('<div class="form-group"/>');
-		var label = $('<label class="control-label"></label>');
-		var input = $('<input class="form-control"/>');
-		var adder = $('<div class="adder"/>');
-
-		console.log(adderNum);
-		formGroup.append(label).append(input);
-		var name = formGroup.clone();
-		var cost = formGroup.clone();
-		var label = "adders[" + adderNum + "]";
-
-		name.find('label').attr('for', label + '[name]').text("Adder Name")
-		name.find('input').attr("placeholder", "Adder Name").attr("name", label + "[name]").attr("type", "text");
-
-		cost.find('label').attr('for', label + '[cost]').text("Adder Cost")
-		cost.find('input').attr("placeholder", "Adder Cost").attr("name", label + "[cost]").attr("type", "number");
-
-		adder.append(name).append(cost).append('<hr/>').appendTo(adders);
+function calculateCost() {
+	var cost = 0;
+	$(':checked:visible').add(':selected').each(function() {
+		cost += $(this).data().cost;
 	});
 
-	$('.remove-adder').click(function() {
-		$(this).parent().remove();
+	var bonusToRoll = $('[name=bonusToRoll]:visible');
+	if(bonusToRoll.length !== 0)
+		cost += (bonusToRoll.val() * bonusToRoll.data().cost);
+
+	$('#talent-cost').text(cost);
+	$('[name=cost]').val(cost);
+}
+$(document).ready(function() {
+	$('#talentID').change(function() {
+		var talent = $(this).children(':selected').text();
+		$('.bonus-to-roll').add('.adders').each(function() {
+			var data = $(this).data();
+			if(data.talent == talent)
+				$(this).fadeIn().find('input').prop('disabled', false);
+			else if($(this).filter(':visible').length > 0)
+				$(this).fadeOut(function() {
+					calculateCost();
+				}).find('input').prop('disabled', true);
+		});
+
+		calculateCost();
+	});
+
+	$('input').change(function() {
+		calculateCost();
 	});
 });
