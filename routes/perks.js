c9857ff1673aa29003af0cf982b60274546de88b
jakofranko <jacob.d.franklin@gmail.com>
2015-11-04 09:28:01 -0700

    updated perks routes to use a better routing method

--- routes/perks.js
+++ routes/perks.js
@@ -5,41 +5,43 @@ var users = require('../models/users');
 var characters = require('../models/characters');
 var perks = require('../models/perks');
 
+// Make sure that the user is an Admin
+router.use(helpers.isAdmin);
 
 // Gets -------------------------------
-router.get('/', helpers.isAdmin, function(req, res) {
+router.get('/', function(req, res) {
 	perks.listPerks(function(err, results) {
 		if(err) throw new Error(err);
 		else res.render('perkTypes/view', { title: 'Perks', perks: results, session: req.session });
 	});
 });
 
-router.get('/add', helpers.isAdmin, function(req, res) {
+router.get('/add', function(req, res) {
 	res.render('perkTypes/add', { title: 'New Perk', session: req.session });
 });
 
-router.get('/edit/:perkID', helpers.isAdmin, function(req, res) {
+router.get('/edit/:perkID', function(req, res) {
 	perks.getPerk({ _id: req.params.perkID }, function(err, perk) {
 		res.render('perkTypes/edit', { title: 'Edit Perk: ' + perk.name, session: req.session, perk: perk });
 	});
 });
 
 // Gets -------------------------------
-router.post('/addPerk', helpers.isAdmin, function(req, res) {
+router.post('/addPerk', function(req, res) {
 	perks.createPerk(req.body.name, { minCost: Number(req.body.minCost), maxCost: Number(req.body.maxCost) }, function(err, result) {
 		if(err) throw new Error(err);
 		else res.redirect('/perks');
 	});
 });
 
-router.post('/updatePerk', helpers.isAdmin, function(req, res) {
+router.post('/updatePerk', function(req, res) {
 	perks.updatePerk({ _id: req.body.perkID }, { name: req.body.name, minCost: Number(req.body.minCost), maxCost: Number(req.body.maxCost) }, function(err, result) {
 		if(err) throw new Error(err);
 		else res.redirect('/perks');
 	});
 });
 
-router.post('/deletePerk', helpers.isAdmin, function(req, res) {
+router.post('/deletePerk', function(req, res) {
 	perks.deletePerk(req.body.perkID, function(err, result) {
 		if(err) throw new Error(err);
 		else res.redirect('/perks');
