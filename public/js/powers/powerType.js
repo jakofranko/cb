16f1dead3622653c88f193d28048b053c7898da6
jakofranko <jacob.d.franklin@gmail.com>
2015-11-05 12:51:21 -0700

    Added some templates and JS for adding and editing powers
    https://app.asana.com/0/47316382927622/62563300653705
    https://app.asana.com/0/47316382927622/62563300653719
    https://app.asana.com/0/47316382927622/62563300653723

---
+++ public/js/powers/powerType.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	var varients = $('#varients');
+	$('.add-varient').click(function() {
+		var varientCount = $('.varient').length;
+		console.log(varientCount);
+		var name = "varient[" + Number(varientCount) + "][name]";
+		var cost = "varient[" + Number(varientCount) + "][cost]";
+		var description = "varient[" + Number(varientCount) + "][description]";
+
+		var varient = $('<div class="varient"></div>');
+		var formGroup = $('<div class="form-group"></div>');
+		var label = $('<label class="control-label"></label>');
+		var input = $('<input/>');
+
+		var nameGroup = formGroup.clone();
+		var nameLabel = label.clone().attr('for', name).addClass('control-label').text('Varient Name').appendTo(nameGroup);
+		var nameInput = input.clone().attr('name', name).attr('id', name).attr('type', 'text').attr('placeholder', 'E.G., 50% Resistant').addClass('form-control').appendTo(nameGroup);
+		nameGroup.appendTo(varient);
+
+		var costGroup = formGroup.clone();
+		var costLabel = label.clone().attr('for', cost).addClass('control-label').text('Varient Cost (optional)').appendTo(costGroup);
+		var costInput = input.clone().attr('name', cost).attr('id', cost).attr('type', 'number').attr('placeholder', 'E.G., 5').addClass('form-control').appendTo(costGroup);
+		costGroup.appendTo(varient);
+
+		var descriptionGroup = formGroup.clone();
+		var descriptionLabel = label.clone().attr('for', description).addClass('control-label').text('Varient Description (optional)').appendTo(descriptionGroup);
+		var descriptionInput = input.clone().attr('name', description).attr('id', description).attr('type', 'text').attr('placeholder', 'E.G., Versus Energy Defenses').addClass('form-control').appendTo(descriptionGroup);
+		descriptionGroup.appendTo(varient);
+
+		varient.appendTo(varients);
+	});
+});
--- routes/powers.js
+++ routes/powers.js
@@ -5,9 +5,37 @@ var users = require('../models/users');
 var characters = require('../models/characters');
 var powers = require('../models/powers');
 
-router.get('/', helpers.isAdmin, function(req, res) {
+// Only Admins allowed
+router.use(helpers.isAdmin);
+
+// GETS
+router.get('/', function(req, res) {
 	powers.listPowers(function(err, powers) {
-		res.render('powerTypes/view', {title: 'Powers', powers: powers, username: req.session.username});
+		if(err) throw new Error(err);
+		else res.render('powerTypes/view', {title: 'Powers', powers: powers, username: req.session.username});
+	});
+});
+
+router.get('/add', function(req, res) {
+	res.render('powerTypes/add', {title: 'New Power'});
+});
+
+
+// POSTS
+router.post('/addPower', function(req, res) {
+	var power = {
+		name: req.body.name,
+		duration: req.body.duration,
+		range: req.body.range,
+		target: req.body.target,
+		endurance: (req.body.endurance && req.body.endurance == "true") ? true : false,
+		cost: req.body.cost
+	};
+	console.log(power);
+
+	powers.createPower(power, function(err, success) {
+		if(err) throw new Error(err);
+		else res.redirect('/powers/add');
 	});
 });
 
